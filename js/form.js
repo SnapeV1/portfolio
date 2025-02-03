f508aa2bfa392fd973adce5a09f39c4dd4332235
SnapeV1 <hamada_amine@yahoo.com>
2025-02-03 11:55:14 +0100

    music button

--- js/form.js
+++ js/form.js
@@ -4,35 +4,21 @@ const form = () => {
 
   contactForm.addEventListener("submit", async (e) => {
     e.preventDefault();
-    const form = e.target;
-    const formData = new FormData(form);
     responseMessage.classList.add("open");
     responseMessage.textContent = "Please wait...";
 
-    async function getData() {
-      try {
-        const response = await fetch("mail.php", {
-          method: "POST",
-          body: formData,
-        });
-        if (!response.ok) {
-          responseMessage.textContent = result;
-        }
-
-        const result = await response.text();
-        responseMessage.textContent = result;
-      } catch (error) {
-        console.error(error.message);
-      }
-    }
-
-    getData()
-      .then(
-        setTimeout(() => {
-          responseMessage.classList.remove("open");
-        }, 3000)
-      )
-      .finally(form.reset());
+    setTimeout(() => {
+      responseMessage.textContent = "Your message has been sent successfully!";
+      responseMessage.style.color = "green";
+      contactForm.reset();
+    }, 2000);
+    
+    // Hide message after 3 seconds
+    setTimeout(() => {
+      responseMessage.classList.remove("open");
+      responseMessage.textContent = "";
+    }, 5000);
   });
 };
+
 export default form;
